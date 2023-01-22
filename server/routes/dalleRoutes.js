01c65f3e66cd74af4aa3cdd2916efc04ec5cfb9d
KonsGio <88385243+KonsGio@users.noreply.github.com>
2023-01-22 22:12:44 +0200

    Image requested and transfered to client

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -16,4 +16,24 @@ router.route('/').get((req, res) => {
     res.send('Hello World');
 })
 
+// get the response database 
+router.route('/').post(async (req, res) => {
+    try {
+      const { prompt } = req.body;
+  
+      const aiResponse = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+        response_format: 'b64_json',
+      });
+//   getting the image and send it to the client 
+      const image = aiResponse.data.data[0].b64_json;
+      res.status(200).json({ photo: image });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    }
+  });
+
 export default router;
